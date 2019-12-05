1f112fabf518a68ff53fd89c0edd87d93fa93149
U-USERSAD\ptrinida <ptrinida@LIM-JTZXMF2.usersad.everis.int>
2019-12-04 22:49:28 -0500

    Fourth microservice

---
+++ src/main/java/com/bankcheckingaccount/BankCheckingAccountApplication.java
@@ -0,0 +1,13 @@
+package com.bankcheckingaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCheckingAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCheckingAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankcheckingaccount/BankCheckingAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankcheckingaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCheckingAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
